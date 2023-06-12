b4c4111eddfee4dce1ae7b770befde24497736df
JRVC026 <77721725+JRVC026@users.noreply.github.com>
2023-06-12 10:03:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,166 +1,179 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const countdownTimer = document.getElementById("countdown-timer");
-  const currentTimeDisplay = document.getElementById("current-time");
-  const initialTimeDisplay = document.getElementById("initial-time");
-  const latestTimeDisplay = document.getElementById("latest-time");
-  const controls = document.getElementById("controls");
+const content = document.getElementById('content');
 
-  function formatDate(date) {
-    const options = { year: "numeric", month: "short", day: "numeric", hour: "numeric", minute: "numeric", hour12: true };
-    return date.toLocaleString("en-US", options);
-  }
+function fetchTextFile(file) {
+    return fetch(file)
+        .then(response => response.text())
+        .then(text => text.trim());
+}
 
-  function readTextFile(file, callback) {
-    const rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-      if (rawFile.readyState === 4 && rawFile.status === 200) {
-        if (callback) callback(rawFile.responseText);
-      }
-    };
-    rawFile.send(null);
-  }
-
-  function writeTextFile(file, content, callback) {
-    const rawFile = new XMLHttpRequest();
-    rawFile.open("POST", file, true);
-    rawFile.onreadystatechange = function() {
-      if (rawFile.readyState === 4 && rawFile.status === 200) {
-        if (callback) callback();
-      }
-    };
-    rawFile.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    rawFile.send("content=" + encodeURIComponent(content));
-  }
-
-  function deleteTextFile(file, callback) {
-    const rawFile = new XMLHttpRequest();
-    rawFile.open("DELETE", file, true);
-    rawFile.onreadystatechange = function() {
-      if (rawFile.readyState === 4 && rawFile.status === 200) {
-        if (callback) callback();
-      }
-    };
-    rawFile.send(null);
-  }
-
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function updateCountdownTime(countdownTime) {
-    countdownTimer.textContent = countdownTime;
-  }
-
-  function showReleaseMessage() {
-    countdownTimer.style.display = "none";
-    controls.innerHTML = '<p class="congrats">Congratulations! You are Free!</p><button class="start-again">Start Again!</button>';
-    const startAgainButton = document.querySelector(".start-again");
-    startAgainButton.addEventListener("click", function() {
-      deleteTextFile("min.txt", function() {
-        deleteTextFile("max.txt", function() {
-          deleteTextFile("cur.txt", function() {
-            location.reload();
-          });
-        });
-      });
+function writeTextFile(file, text) {
+    return fetch(file, {
+        method: 'PUT',
+        body: text,
+        headers: {
+            'Content-Type': 'text/plain'
+        }
     });
-  }
+}
 
-  function startCountdown(releaseTime) {
-    const countdownInterval = setInterval(function() {
-      const currentTime = new Date().getTime();
-      const timeRemaining = releaseTime - currentTime;
-      if (timeRemaining <= 0) {
-        clearInterval(countdownInterval);
-        showReleaseMessage();
-      } else {
-        const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
-        const seconds = Math.floor((timeRemaining / 1000) % 60);
-        const countdownTime = hours.toString().padStart(2, "0") + ":" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
-        updateCountdownTime(countdownTime);
-      }
-    }, 1000);
-  }
+function deleteTextFile(file) {
+    return fetch(file, { method: 'DELETE' });
+}
 
-  function initializeCountdown() {
-    // Read the contents of 'cur.txt'
-    readTextFile("cur.txt", function(curContent) {
-      const curTime = parseInt(curContent);
-      const currentTime = new Date().getTime();
+function padZero(num) {
+    return num.toString().padStart(2, '0');
+}
 
-      // Check if 'cur.txt' is empty or the current time is past the release time
-      if (isNaN(curTime) || curTime <= currentTime) {
-        // Read the contents of 'min.txt' and 'max.txt'
-        readTextFile("min.txt", function(minContent) {
-          readTextFile("max.txt", function(maxContent) {
-            const minTime = parseInt(minContent);
-            const maxTime = parseInt(maxContent);
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = padZero(date.getMonth() + 1);
+    const day = padZero(date.getDate());
+    const hour = padZero(date.getHours());
+    const minute = padZero(date.getMinutes());
+    return `${year}-${month}-${day} ${hour}:${minute}`;
+}
 
-            // Show the input fields and "BEGIN" button
-            const minInput = document.createElement("input");
-            minInput.type = "number";
-            minInput.placeholder = "Minimum hours";
-            minInput.addEventListener("input", function() {
-              this.value = this.value.replace(/\D/g, "").slice(0, 3); // Only allow numbers and limit to 3 digits
-            });
+function formatTime(seconds) {
+    const days = Math.floor(seconds / 86400);
+    seconds %= 86400;
+    const hours = Math.floor(seconds / 3600);
+    seconds %= 3600;
+    const minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+    return `${days}d ${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+}
 
-            const maxInput = document.createElement("input");
-            maxInput.type = "number";
-            maxInput.placeholder = "Maximum hours";
-            maxInput.addEventListener("input", function() {
-              this.value = this.value.replace(/\D/g, "").slice(0, 3); // Only allow numbers and limit to 3 digits
-            });
+function updateTimer(releaseDate) {
+    const countdownElem = document.querySelector('.countdown');
+    const now = new Date();
+    
+    if (now >= releaseDate) {
+        clearInterval(timerInterval);
+        content.innerHTML = `
+            <p>Congratulations! You are Free!</p>
+            <button id="reset">Start Again!</button>
+        `;
+        document.getElementById('reset').addEventListener('click', resetTimer);
+        return;
+    }
+    
+    const timeLeft = Math.round((releaseDate - now) / 1000);
+    
+    countdownElem.textContent = formatTime(timeLeft);
+}
 
-            const beginButton = document.createElement("button");
-            beginButton.textContent = "BEGIN";
-            beginButton.addEventListener("click", function() {
-              const minHours = parseInt(minInput.value);
-              const maxHours = parseInt(maxInput.value);
-
-              // Validate the input values
-              if (isNaN(minHours) || isNaN(maxHours) || minHours <= 0 || maxHours <= 0 || minHours === maxHours) {
-                alert("Invalid input. Please enter valid values for the minimum and maximum hours.");
-              } else {
-                const minReleaseTime = currentTime + minHours * 60 * 60 * 1000;
-                const maxReleaseTime = currentTime + maxHours * 60 * 60 * 1000;
-
-                // Write the release time to 'min.txt' and 'cur.txt'
-                writeTextFile("min.txt", minReleaseTime, function() {
-                  writeTextFile("cur.txt", minReleaseTime, function() {
-                    location.reload();
-                  });
-                });
-
-                // Write the release time to 'max.txt'
-                writeTextFile("max.txt", maxReleaseTime, function() {
-                  location.reload();
-                });
-              }
-            });
-
-            controls.appendChild(minInput);
-            controls.appendChild(maxInput);
-            controls.appendChild(beginButton);
-          });
-        });
-      } else {
-        // 'cur.txt' is not empty and the current time is before the release time
-
-        // Read the release time from 'cur.txt'
-        const releaseTime = parseInt(curContent);
-
-        // Update the countdown display
-        updateCountdownTime(formatDate(new Date(releaseTime)));
-
-        // Start the countdown
-        startCountdown(releaseTime);
-      }
+function startTimer() {    Promise.all([
+        fetchTextFile('min.txt'),
+        fetchTextFile('max.txt'),
+        fetchTextFile('cur.txt')
+    ]).then(([minDateText, maxDateText, curDateText]) => {
+        const minDate = new Date(minDateText);
+        const maxDate = new Date(maxDateText);
+        let curDate = new Date(curDateText);
+        
+        const now = new Date();
+        
+        if (now >= curDate) {
+            const timePassed = now - curDate;
+            curDate = new Date(now.getTime() + timePassed);
+            writeTextFile('cur.txt', curDate.toISOString());
+        }
+        
+        if (curDate - now <= 3600000 && maxDate - now >= 3600000) {
+            const hoursLeft = Math.round((maxDate - now) / 3600000);
+            const hoursToAdd = Math.floor(Math.random() * hoursLeft);
+            curDate = new Date(now.getTime() + hoursToAdd * 3600000);
+            writeTextFile('cur.txt', curDate.toISOString());
+        }
+        
+        if (maxDate - curDate <= 10800000) {
+            curDate = maxDate;
+            writeTextFile('cur.txt', curDate.toISOString());
+        }
+        
+        content.innerHTML = `
+            <div class="countdown"></div>
+            <div class="release-date">
+                Countdown Until Release:<br>
+                ${formatDate(curDate)}
+            </div>
+            <div class="release-date">
+                Initial Release Date/Time:<br>
+                ${formatDate(minDate)}
+            </div>
+            <div class="release-date">
+                Latest Release Date/Time:<br>
+                ${formatDate(maxDate)}
+            </div>
+        `;
+        
+        timerInterval = setInterval(() => updateTimer(curDate), 1000);
     });
-  }
+}
 
-  // Initialize the countdown on page load
-  initializeCountdown();
+function resetTimer() {
+    Promise.all([
+        deleteTextFile('min.txt'),
+        deleteTextFile('max.txt'),
+        deleteTextFile('cur.txt')
+    ]).then(() => location.reload());
+}
+
+function validateInput() {
+    const minInput = document.getElementById('min-input');
+    const maxInput = document.getElementById('max-input');
+    const beginButton = document.getElementById('begin');
+    
+    const min = parseInt(minInput.value, 10);
+    const max = parseInt(maxInput.value, 10);
+    
+    if (isNaN(min) || isNaN(max) || min >= max) {
+        beginButton.disabled = true;
+    } else {
+        beginButton.disabled = false;
+    }
+}
+
+function startCountdown() {
+    const minInput = document.getElementById('min-input');
+    const maxInput = document.getElementById('max-input');
+    
+    const minHours = parseInt(minInput.value, 10);
+    const maxHours = parseInt(maxInput.value, 10);
+    
+    const now = new Date();
+    
+    const minReleaseTime = now.getTime() + minHours * 3600000;
+    const maxReleaseTime = now.getTime() + maxHours * 3600000;
+    
+    Promise.all([
+        writeTextFile('min.txt', new Date(minReleaseTime).toISOString()),
+        writeTextFile('max.txt', new Date(maxReleaseTime).toISOString()),
+        writeTextFile('cur.txt', new Date(minReleaseTime).toISOString())
+    ]).then(() => location.reload());
+}
+
+let timerInterval;
+
+fetchTextFile('cur.txt').then(curDateText => {
+    if (curDateText) {
+        startTimer();
+    } else {
+        content.innerHTML = `
+            <label>
+                Minimum Hours:
+                <input type="number" id="min-input" min="1" max="999" required>
+            </label><br><br>
+            <label>
+                Maximum Hours:
+                <input type="number" id="max-input" min="1" max="999" required>
+            </label><br><br>
+            <button id="begin" disabled>Begin</button>
+        `;
+        
+        document.getElementById('min-input').addEventListener('input', validateInput);
+        document.getElementById('max-input').addEventListener('input', validateInput);
+        
+        document.getElementById('begin').addEventListener('click', startCountdown);
+    }
 });
-
